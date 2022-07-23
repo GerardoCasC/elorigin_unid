8170e27de3a496beb7fb2ac7cc4a9f7400e417a8
andres_realpozo <bollofrances@gmail.com>
2022-07-23 22:20:46 +0100

    Revert "Lesgo"
    This reverts commit 7e75acc29fe79bb1b9ee91a71ecd15df34c7b18f.

--- lib/functions.php
+++ lib/functions.php
@@ -6,13 +6,4 @@ function get_all_users($connect){
     $resultado = mysqli_query($connect, $consulta);
     return $resultado;
     }
-
-    //aqui va la parte de andres
-    function get_all_categories($connect){  
-        $consulta = "SELECT * FROM categories" ;
-        $resultado = mysqli_query($connect, $consulta);
-        
-        return $resultado;
-        
-        }
 ?>
